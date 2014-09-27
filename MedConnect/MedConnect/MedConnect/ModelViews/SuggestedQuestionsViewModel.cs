28076fe0c4dc46d348f61a1bb57c343c674e736a
BecLai <becdotlai@gmail.com>
2014-09-27 18:11:37 -0400

    Set up more skeleton classes

---
+++ MedConnect/MedConnect/MedConnect/ModelViews/SuggestedQuestionsViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel; 
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MedConnect.Models; 
+
+namespace MedConnect.ModelViews
+{
+    public class SuggestedQuestionsViewModel
+    {
+        public ObservableCollection<Question> SuggestedQuestions { get; set; }
+
+        public SuggestedQuestionsViewModel() 
+        {
+            SuggestedQuestions = new ObservableCollection<Question>();
+        }
+    }
+}
--- MedConnect/MedConnect/MedConnect/Views/SuggestedQuestionsPage.cs
+++ MedConnect/MedConnect/MedConnect/Views/SuggestedQuestionsPage.cs
@@ -3,10 +3,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Xamarin.Forms;
 
 namespace MedConnect.Views
 {
-    public class SuggestedQuestionsPage
+    public class SuggestedQuestionsPage : ContentPage 
     {
+        public SuggestedQuestionsPage() 
+        {
+            Title = "Suggested Questions";
+            
+            var list = new ListView();
+            Content = list; 
+
+        }
     }
 }
