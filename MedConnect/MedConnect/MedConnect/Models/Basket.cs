9103cf528980620b2a1e1ebc074bd0c26e667d1f
Kevin Oh <jo63@duke.edu>
2014-10-13 01:15:12 -0400

    fixed basket model to reflect questions collection

--- MedConnect/MedConnect/MedConnect/Models/Basket.cs
+++ MedConnect/MedConnect/MedConnect/Models/Basket.cs
@@ -1,12 +1,25 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MedConnect.Models;
 
 namespace MedConnect
 {
 	public class Basket
 	{
+        public ObservableCollection<Question> Questions { get; set; }
+
 		public Basket ()
 		{
+            Questions = new ObservableCollection<Question>();
 		}
+
+        public void addQuestion(Question toAdd) {
+            Questions.Add(toAdd); 
+        }
 	}
 }
 
